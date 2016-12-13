44de307a6a764ff2cfccf0b79fcb3fb3d1e58684
Allison <allisonmariebrowne@gmail.com>
2016-12-12 19:59:12 -0500

    Add Dockerfile and configure logging to standard out for docker

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module CommuteCalculator
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # send logging to STDOUT for Docker
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 end
